c56d6002e084b42c43de833c96d0663b9d04812e
Nick Delfino <delfinonick@gmail.com>
2015-04-11 12:37:06 -0400

    Revert "Revert "String thing""
    This reverts commit 363a085e7bc48dd68b1d760af3366947bb3c44d8.

--- app/src/main/java/rccarcontroller/hackathon/umass/rccarcontroller/MainActivity.java
+++ app/src/main/java/rccarcontroller/hackathon/umass/rccarcontroller/MainActivity.java
@@ -28,10 +28,13 @@ public class MainActivity extends ActionBarActivity {
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
+            String name = "";
             // When discovery finds a device
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
                 // Get the BluetoothDevice object from the Intent
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+
                 // Add the name and address to an array adapter to show in a ListView
                 mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
             }
